5e8efbf2e71d6227c514c0af55fa92feb9987af4
cJ barnes <mail@cjbarnes.co.uk>
2015-01-18 20:36:39 +0000

    Remove jQuery UI datepicker script

--- trunk/admin/js/periodicalpress-admin.js
+++ trunk/admin/js/periodicalpress-admin.js
@@ -10,22 +10,7 @@
 	 */
 	$( function initPeriodicalPress() {
 
-		/**
-		 * Initialize datepickers with options.
-		 *
-		 * @see {@link http://api.jqueryui.com/datepicker/}
-		 */
-		$( '.pp-datepicker' ).datepicker({
-			altFormat: 'yy-mm-dd',
-			changeMonth: true,
-			changeYear: true,
-			currentText: l10n.datepickerCurrentText,
-			dateFormat: l10n.datepickerDateFormat,
-			firstDay: 1,
-			hideIfNoPrevNext: true,
-			isRTL: ( 'true' === l10n.isRTL ? true : false ),
-			showButtonPanel: true
-		});
+
 
 	} );
 
